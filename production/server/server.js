a63e8279a1980a8d7dce93d9519c1af96ac5b034
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-13 08:48:43 -0800

    initialized modules in server.js, displayed 'hello world' on localhost:5000

--- production/server/server.js
+++ production/server/server.js
@@ -1,3 +1,17 @@
 const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const helmet = require('helmet'); // enhances security in Express
+const morgan = require('morgan'); // logs all HTTP requests
+
+const app = express();
+
+app.use(helmet()); 
+app.use(morgan('tiny')) // 'tiny' condenses logged HTTP requests
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+// Express will run on port 5000 and React will run on 3000
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
+})
