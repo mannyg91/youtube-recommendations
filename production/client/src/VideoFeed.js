1589539ab2c428ab9d2ba1a77fefaec6b3673021
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-15 12:04:55 -0800

    added useEffect for rapidAPI fetch

--- production/client/src/VideoFeed.js
+++ production/client/src/VideoFeed.js
@@ -3,7 +3,24 @@ import Video from './Video'
 
 const VideoFeed = () => {
 
-  
+  React.useEffect(() => {
+    console.log("Effect ran");
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://youtube-v31.p.rapidapi.com/captions?part=snippet&videoId=M7FIvfx5J10', options)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+      })
+      .catch(err => console.error(err));
+  }, []);
 
   return (
     <>
