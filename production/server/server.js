bfa61716edc63add3403bd6c5a267ce78789e912
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-19 10:44:55 -0800

    configured server.js for google oauth

--- production/server/server.js
+++ production/server/server.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const helmet = require('helmet'); // enhances security in Express
 const morgan = require('morgan'); // logs all HTTP requests
+const cors = require("cors"); //for cross-origin resource sharing
+const { OAuth2Client } = require("google-auth-library");
+const jwt = require("jsonwebtoken");
+require("dotenv/config");
 
 const app = express();
 
 app.use(helmet()); 
 app.use(morgan('tiny')) // 'tiny' condenses logged HTTP requests
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-})
+
+app.use(
+    cors({
+      origin: ["http://localhost:3000"],
+      methods: "GET,POST,PUT,DELETE,OPTIONS",
+    })
+  );
+
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//     res.send('hello world');
+// })
+
+let DB = [];
 
 // Express will run on port 5000 and React will run on 3000
 app.listen(5000, () => {
     console.log("Server started on port 5000");
 })
+
