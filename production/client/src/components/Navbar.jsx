74b282954eadbc7a49c3bfb12b53d91092293ada
Ashley B <butela.ashley@gmail.com>
2023-01-25 17:44:56 -0500

    Main react app cleaned up

--- production/client/src/components/Navbar.jsx
+++ production/client/src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import WavesIcon from '@mui/icons-material/Waves';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuItem from '@mui/material/MenuItem';
