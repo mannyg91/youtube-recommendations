08d27e3d33e95d1347c62c2319e3ab2ad2470189
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-25 10:56:50 -0800

    installed material-ui, added navbar, added constants file for later refactoring

--- production/client/src/components/Navbar.jsx
+++ production/client/src/components/Navbar.jsx
@@ -1,15 +1,100 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import WavesIcon from '@mui/icons-material/Waves';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { createTheme } from '@mui/material/styles';
 
-const Navbar = () => {
+
+export default function Navbar() {
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#000000',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <header>
-      <nav>
-        {/* <Link to="/signup">Sign Up</Link>
-        <Link to="/login">Login</Link> */}
-      </nav>
-    </header>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar id="MenuAppBar">
+          <IconButton
+            id="burger"
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
+            <WavesIcon />
+          </IconButton>
+          <Typography id="logo" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            ZepBox
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>Log Out</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Navbar
