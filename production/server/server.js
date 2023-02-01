1b2ec4713951c939d51e431294e85a5f11017201
Manny Gomez <mjgomez@nevada.unr.edu>
2023-02-01 10:51:25 -0800

    basic server.js file setup with express

--- production/server/server.js
+++ production/server/server.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT
+
+const app = express()
+
+app.get('/api/blockedChannels', (req, res) => {
+  res.send('Get blocked channel IDs')
+})
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`)
+})
