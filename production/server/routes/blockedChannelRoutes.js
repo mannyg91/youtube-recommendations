02c3c1e6d76f70269893f363f66a5bd61217ec3b
Manny Gomez <mjgomez@nevada.unr.edu>
2023-02-01 12:43:55 -0800

    added controller for routes

--- production/server/routes/blockedChannelRoutes.js
+++ production/server/routes/blockedChannelRoutes.js
@@ -1,29 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { 
+  getBlockedChannels,
+  setBlockedChannels,
+  updateBlockedChannels,
+  deleteBlockedChannels
+} = require('../controllers/blockedChannelController')
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Get blocked channel'
-  })
-}) 
-
-router.post('/', (req, res) => {
-  res.status(200).json({
-    message: 'Set blocked channel'
-  })
-}) 
-
-router.put('/:id', (req, res) => {
-  res.status(200).json({
-    message: `Update blocked channel ${req.params.id}`
-  })
-}) 
-
-router.delete('/:id', (req, res) => {
-  res.status(200).json({
-    message: `Delete blocked channel ${req.params.id}`
-  })
-}) 
-
+router.get('/', getBlockedChannels) 
+router.post('/', setBlockedChannels) 
+router.put('/:id', updateBlockedChannels)
+router.delete('/:id', deleteBlockedChannels)
 
 module.exports =  router
