6a5fdbd9c315e9575947f0fe99285b10315aba27
Manny Gomez <mjgomez@nevada.unr.edu>
2023-02-10 09:04:50 -0800

    disabled testData for deployment

--- production/client/src/hooks/SearchContext.jsx
+++ production/client/src/hooks/SearchContext.jsx
@@ -7,14 +7,14 @@ import data from "../components/data/testData.js";
 const SearchContext = React.createContext();
 
 const SearchContextProvider = (props) => {
-  const [videos, setVideos] = React.useState(data.items); // change null to data.items for testData
+  const [videos, setVideos] = React.useState(null); // change null to data.items for testData
   const [searchTerm, setSearchTerm] = React.useState("history");
   const [selectedTopic, setSelectedTopic] = React.useState("/m/04rlf");// music topic
   const [sliderState, setSliderState] = React.useState(2);
   const [focusKeywords, setFocusKeywords] = React.useState(null);
 
   React.useEffect(() => {
-    const usingTestData = true;
+    const usingTestData = false;
         if (!usingTestData) { 
           const getVideos = async () => {
             const data = await youtubeSearch(searchTerm, selectedTopic);
