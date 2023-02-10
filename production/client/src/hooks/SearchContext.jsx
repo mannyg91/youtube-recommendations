7143f4621481aa39936d2cc9b99d3c490c6878c2
Manny Gomez <mjgomez@nevada.unr.edu>
2023-02-09 17:19:00 -0800

    corrected testData path

--- production/client/src/hooks/SearchContext.jsx
+++ production/client/src/hooks/SearchContext.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
 import { youtubeSearch } from '../utils/getVideoResults'
 import appTopics  from '../components/data/topics';
-import data from '../data/testData'
+import data from "../components/data/testData.js";
 
 
 const SearchContext = React.createContext();
 
 const SearchContextProvider = (props) => {
-  const [videos, setVideos] = React.useState(null); // change null to data.items for testData
+  const [videos, setVideos] = React.useState(data.items); // change null to data.items for testData
   const [searchTerm, setSearchTerm] = React.useState("history");
   const [selectedTopic, setSelectedTopic] = React.useState("/m/04rlf");// music topic
   const [sliderState, setSliderState] = React.useState(2);
   const [focusKeywords, setFocusKeywords] = React.useState(null);
 
   React.useEffect(() => {
-    const usingTestData = false;
+    const usingTestData = true;
         if (!usingTestData) { 
           const getVideos = async () => {
             const data = await youtubeSearch(searchTerm, selectedTopic);
