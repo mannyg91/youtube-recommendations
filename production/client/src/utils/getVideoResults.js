13b11984e868f7957790171156cb78689adfc3af
Manny Gomez <mjgomez@nevada.unr.edu>
2023-02-15 06:37:33 -0800

    added optional chaining on return statement as needed to resolve API calls not loading consistently

--- production/client/src/utils/getVideoResults.js
+++ production/client/src/utils/getVideoResults.js
@@ -4,7 +4,7 @@ export const youtubeSearch = async (query, topicId, type) => {
 
   function loadClient() {
     gapi.client?.setApiKey(process.env?.REACT_APP_YOUTUBE_API_KEY);
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+    return gapi.client?.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
       .then(function () { console.log("GAPI client loaded for API"); },
         function (err) { console.error("Error loading GAPI client for API", err); });
   }
