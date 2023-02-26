3be01b17d0e971a456025967e3be5a19cd703106
Manny Gomez <mjgomez@nevada.unr.edu>
2023-02-26 08:26:21 -0800

    modified email to use watchwise.tv domain

--- production/server/email.js
+++ production/server/email.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 
 function createTransporter() {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.zoho.com',
+    host: 'mail.privateemail.com',
     port: 465,
     secure: true,
     auth: {
@@ -17,12 +17,11 @@ function createTransporter() {
 
 function sendPasswordResetEmail(email, passwordResetToken) {
   const resetLink = `${process.env.RESET_PAGE}?token=${passwordResetToken}`;
-
   const mailOptions = {
     from: process.env.EMAIL,
     to: email,
     subject: 'Password Reset',
-    text: `Hey! Please click the following link to reset your password: ${resetLink}`
+    html: `Hey! Please click <a href="${resetLink}">here</a> to reset your password.`
   };
 
   const transporter = createTransporter();
